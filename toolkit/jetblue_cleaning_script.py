d9258c41a2f83914cd4436fab3ac05a2a339990d
Chris Kuzemka <cmkuzemka@gmail.com>
2024-11-04 19:25:08 -0500

    copy pasted code on the cleaning script from main into toolkit_updates branch. for some reason the changes didn't hold on the merge. weird.

--- toolkit/jetblue_cleaning_script.py
+++ toolkit/jetblue_cleaning_script.py
@@ -5,8 +5,24 @@ import math
 import cmath
 import matplotlib.pyplot as plt
 import seaborn as sns
+from datetime import datetime as dt
+
+
 
 ##Functions
+#Datetime Conversion
+def table_datetime_conversion(df):
+    # Convert 'searchDate' and 'flightDate' to datetime, handling potential errors
+    df['searchDate'] = pd.to_datetime(df['searchDate'], format='%Y-%m-%d', errors='coerce')
+    df['flightDate'] = pd.to_datetime(df['flightDate'], format='%Y-%m-%d', errors='coerce')
+
+    # Ensure the columns are of datetime type
+    df['searchDate'] = df['searchDate'].astype('datetime64[ns]')
+    df['flightDate'] = df['flightDate'].astype('datetime64[ns]')
+
+    return df
+
+#TODO: for days between creation
 def extract_time(time_string):
     segments = time_string.split("||")
     first_segment = segments[0]
@@ -19,9 +35,13 @@ def kpi_creation(df):
     df["daysLeft"] = df["flightDate"] - df["searchDate"].dt.days
     # df["dollarPerMile"]
     # df["milePerDollar"]
-    # df["monthly_average"]
+    # df["monthlyAverageRoutePrice"]
 
     return df
+
+
+
+
 
 ##Bring in table
 relative_path_to_native_data = "../data/jetblue_df.csv"
@@ -42,21 +62,12 @@ df = df[ df["segmentsAirlineName"].isin(pure_jetblue_filter) ]
 ##Label cleaning
 
 #Datetime Conversion
-# Convert 'searchDate' and 'flightDate' to datetime, handling potential errors
-df['searchDate'] = pd.to_datetime(df['searchDate'], format='%Y-%m-%d', errors='coerce')
-df['flightDate'] = pd.to_datetime(df['flightDate'], format='%Y-%m-%d', errors='coerce')
-
-# Ensure the columns are of datetime type
-df['searchDate'] = df['searchDate'].astype('datetime64[ns]')
-df['flightDate'] = df['flightDate'].astype('datetime64[ns]')
-
-
+df = table_datetime_conversion(df)
 
 ##Feature Engineering
 df = kpi_creation(df)
 
 ##Feature reduction
-#drop redundant airline codes and redundant airline names
 #drop redundant airline codes and redundant airline names
 
 ##Save Table into data folder
